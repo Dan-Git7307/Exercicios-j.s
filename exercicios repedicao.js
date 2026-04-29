12f015e65284a1a9af3303d2ee454a37690f7696
Daniel Pereira da Silva <danielsilvasilva7307@gmail.com>
2026-04-29 19:38:22 -0300

    exercicios feitio

--- exercicios repedicao.js
+++ exercicios repedicao.js
@@ -11,10 +11,33 @@ for (let i = 1; i <= 10; i++) {
 }*/
 
 //Exercicio 3
-const N = Number(prompt("Digite um numero"));
+/*const N = Number(prompt("Digite um numero"));
 let soma = 0;
 
 for (let i = 1; i <= N; i++) {
   soma += i;
 }
-console.log(`Soma de 1 ate ${N} = ${soma}`);
+console.log(`Soma de 1 ate ${N} = ${soma}`);*/
+
+
+/*Exercicio 1 Intermediario
+/*console.log("numeros pares de 0 a 50\n");
+for (let i = 1; i <= 50; i++) {
+    if (i % 2 === 0) {
+      console.log(i);           
+}
+
+}*/
+
+//contagem regressiva de 10 ate 0
+
+let tempo = 10;
+console.log("Iniciando contagem regressiva...");
+
+while (tempo > 0) {
+ console.log('${tempo}...' + tempo);
+ 
+ tempo--
+
+}
+console.log("Deu certo!!")
